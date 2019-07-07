23fe6a0ae109eb89202cdf782db1825ed94de762
Larry McCay IV <46705753+lmccay4@users.noreply.github.com>
2019-07-06 23:55:01 -0400

    Add files via upload

---
+++ NJHouseValue.R
@@ -0,0 +1,30 @@
+library(tidycensus)
+library(mapdeck)
+library(tidyverse)
+
+
+census_api_key("", install = TRUE)
+
+mapDeckToken <- ""
+
+census <- load_variables(2017, "acs5")
+view(census)
+
+#B25077_001 median house value
+#B20002_001 median earnings past 12 months
+
+housingData <- get_acs(geography = "tract", 
+              variables = "B25077_001", 
+              state = "NJ", 
+              geometry = TRUE) %>%
+  drop_na(estimate) %>%
+  mutate(elev = estimate/100)
+housingData
+
+mapdeck(token = mapDeckToken, style = 'mapbox://styles/mapbox/dark-v9', pitch = 50, zoom = 15) %>%
+  add_polygon(data = housingData, 
+              fill_colour = "estimate", 
+              tooltip = "estimate", 
+              layer = "nj_housing", 
+              elevation = "elev",
+              legend = TRUE)
